b05375a5abe503b76947e70575036ec506efc4b7
JonPizza <york.jon.2005@gmail.com>
2020-01-28 20:40:36 -0500

    change install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -1,3 +1,2 @@
 wget "https://github.com/trexminer/T-Rex/releases/download/0.14.6/t-rex-0.14.6-linux-cuda10.0.tar.gz" -q -P /mine/miners/
 tar -C /mine/miners -xzf /mine/miners/t-rex-0.14.6-linux-cuda10.0.tar.gz
-mv /mine/miners/t-rex /mine/miners/trex
